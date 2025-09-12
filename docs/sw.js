891b0d180e63a65f1ea9c4664960588a3da1da01
ghostfriend1 <therealtylerogrady@gmail.com>
2025-09-12 14:46:32 -0500

    Delete docs/sw.js

--- docs/sw.js
+++
@@ -1,6 +0,0 @@
-
-const V='cni-neon-v1';
-const ASSETS=['./','./index.html','./cni_neon_production.css','./manifest.webmanifest','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==V).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(V).then(c=>c.put(e.request,copy));return res;}).catch(()=>r)))});
